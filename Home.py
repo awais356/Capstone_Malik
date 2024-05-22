8cbe93cc3e2b2373fe27b93b72ec254ea181ac3d
awais356 <30800412+awais356@users.noreply.github.com>
2024-05-22 13:30:02 -0230

    Update Home.py

--- Home.py
+++ Home.py
@@ -15,7 +15,7 @@ logo = "https://i.imgur.com/UbOXYAU.png"
 st.sidebar.image(logo)
 
 # Customize page title
-st.title("Streamlit for Geospatial Applications")
+st.title("Application for data visualization")
 
 st.markdown(
     """
